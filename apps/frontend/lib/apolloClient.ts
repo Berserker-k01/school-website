1f7501b168af192701a14151ffb2122ccee590d1
Berserker <155231526+Berserker-k01@users.noreply.github.com>
2026-05-09 14:05:07 +0100

    g

--- apps/frontend/lib/apolloClient.ts
+++ apps/frontend/lib/apolloClient.ts
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 // https://www.apollographql.com/blog/apollo-client/next-js/next-js-getting-started/
 
 const client = new ApolloClient({
-  uri: 'https://strapi.elektronplus.pl/graphql',
+  uri: `${process.env.NEXT_PUBLIC_STRAPI_API_URL || 'https://strapi.elektronplus.pl'}/graphql`,
   cache: new InMemoryCache(),
 });
 
