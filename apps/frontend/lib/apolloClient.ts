60e461b0e24059c48c0c1b6ed6784cc01f8fb13a
Berserker <155231526+Berserker-k01@users.noreply.github.com>
2026-05-13 20:48:00 +0100

    go

--- apps/frontend/lib/apolloClient.ts
+++ apps/frontend/lib/apolloClient.ts
@@ -2,8 +2,17 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 // https://www.apollographql.com/blog/apollo-client/next-js/next-js-getting-started/
 
+const getStrapiURL = () => {
+  // Si on est côté serveur (Node.js)
+  if (typeof window === 'undefined') {
+    return process.env.NEXT_PUBLIC_STRAPI_API_URL_INTERNAL || 'http://strapi:1337';
+  }
+  // Si on est côté client (Navigateur)
+  return process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:13888';
+};
+
 const client = new ApolloClient({
-  uri: `${process.env.NEXT_PUBLIC_STRAPI_API_URL || 'https://strapi.elektronplus.pl'}/graphql`,
+  uri: `${getStrapiURL()}/graphql`,
   cache: new InMemoryCache(),
 });
 
